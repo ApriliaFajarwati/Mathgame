8278a5d062f0ae2d6ffac008f0d93cd015605184
ApriliaFajarwati <57347541+ApriliaFajarwati@users.noreply.github.com>
2020-06-06 20:43:20 +0700

    Create koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,12 @@
+<?php
+// host web server
+$servername       =   "localhost";
+// username untuk mengakses database
+$username   =   "id13979024_root";
+// password untuk mengakses database
+$password       = "=r0n++Z4m^UOZhF3";
+// database yang digunakan
+$dbname   = "id13979024_game";
+// script untuk koneksi ke database
+$koneksi = mysqli_connect($servername, $username, $password, $dbname)
+?>
